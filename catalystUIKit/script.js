0d8a5377a83748be2a76a132666d136aeb51ce95
nikiiigoyal <nikiiigoyal@gmail.com>
2024-10-08 22:59:25 +0530

    project

--- catalystUIKit/script.js
+++ catalystUIKit/script.js
@@ -1,1 +1,11 @@
-// functionality for dark mode
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkThemeToggle');
+    const html = document.documentElement;
+
+    darkModeToggle.addEventListener('click', () => {
+        html.classList.toggle('dark');
+        
+      
+       
+    });
+});

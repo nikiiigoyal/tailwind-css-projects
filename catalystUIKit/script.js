e7a8b5b7aa3e0b234d207f38b761cba8f678ad17
Yash Mittal <yashmittalmm@gmail.com>
2024-10-09 23:59:57 +0530

    remove npm and use cdn

--- catalystUIKit/script.js
+++ catalystUIKit/script.js
@@ -1,11 +1,15 @@
+console.log("HI SCRIPT FILE")
+
+tailwind.config = {
+    darkMode: 'class',
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('darkThemeToggle');
     const html = document.documentElement;
 
     darkModeToggle.addEventListener('click', () => {
+        console.log("BUTTON CLICKED")
         html.classList.toggle('dark');
-        
-      
-       
     });
 });

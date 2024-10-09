c744a80c11ec5710e17f47097206d3c348d7d1ca
nikiiigoyal <nikiiigoyal@gmail.com>
2024-10-09 16:55:19 +0530

    project

--- catalystUIKit/tailwind.config.js
+++ catalystUIKit/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ["./**/*.{html,js}"],
+  darkMode: 'class', // Enables dark mode using a 'class'
+  content: ["./**/*.{html,js}"], // Specifies where Tailwind should look for class usage (all .html and .js files)
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], // Adds 'Inter' font family under the name 'inter'
+      }
+    },
   },
-  plugins: [],
+  plugins: [], // No additional plugins are being used
 }
